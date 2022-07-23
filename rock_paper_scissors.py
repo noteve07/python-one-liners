0452ba24b831d1482c10ad739ee28cc1f1ca8cfc
noteve17 <109844376+noteve17@users.noreply.github.com>
2022-07-23 15:48:48 +0800

    Create rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,4 @@
-# this one line of code took me 2 hours lol
-# 06/28/2022 - 21:20 (utc+8)
+# one liner rock paper scissors
+# 06/28/2022 - 21:20
 
 print('\n'.join([f"{p1} vs {p2}".ljust(10)+(f"Player {(1,2)[dict(P='S',R='P',S='R')[p1]==p2]} won!", "Draw!")[p1==p2] for c in (lambda x: __import__('random').choice(x),)*20 for p1, p2 in zip(c('RPS'), c('RPS'))]))
